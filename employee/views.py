b0ad5858428088d9eb2970271b60d39577afcd0f
ziba zolfaghar <zibazolfaghar@yahoo.com>
2020-10-03 16:11:10 +0330

    Initial commit

--- employee/views.py
+++ employee/views.py
@@ -3,25 +3,23 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from .models import employee
-from.serializers import employeemodelserializer
+from .serializers import employeemodelserializer
 
 
 @api_view(["POST"])
 def post_employee(request):
-    data={
-        'name':request.data['name'],
-        'age':request.data['age'],
-        'salary':request.data['salary'],
-        'post':request.data['post'],
-        'name':request.data['name'],
-        'name':request.data['name']
-
-
+    data = {
+        'name': request.data['name'],
+        'age': request.data['age'],
+        'salary': request.data['salary'],
+        'post': request.data['post'],
+        'name': request.data['name'],
+        'name': request.data['name']
 
     }
-    ser=employeemodelserializer(data=data)
+    ser = employeemodelserializer(data=data)
     if ser.is_valid():
         ser.save()
         Response(ser.errors, status=status.HTTP_201_CREATED)
     else:
-        Response(ser.errors,status=status.HTTP_400_BAD_REQUEST)
+        Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
